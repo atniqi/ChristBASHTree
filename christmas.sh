efc3fb33bbc0b47278fe2f8fcdf33a33df458d03
Atdhe <atniqii@gmail.com>
2020-12-25 18:14:31 +0100

    Small change

--- christmas.sh
+++ christmas.sh
@@ -10,7 +10,7 @@ color=0
 tput setaf 2; tput bold
 
 # Tree
-for ((i=1; i<20; i+=2))
+for ((i=1; i<50; i+=2))
 {
     tput cup $lin $col
     for ((j=1; j<=i; j++))
@@ -24,30 +24,30 @@ for ((i=1; i<20; i+=2))
 tput sgr0; tput setaf 3
 
 # Trunk
-for ((i=1; i<=2; i++))
+for ((i=1; i<=5; i++))
 {
     tput cup $((lin++)) $c
-    echo 'mWm'
+    echo '||'
 }
+
 new_year=$(date +'%Y')
 let new_year++
 tput setaf 1; tput bold
-tput cup $lin $((c - 6)); echo FELICES FIESTAS
-tput cup $((lin + 1)) $((c - 9)); echo Y mucho CODIGO en $new_year
+tput cup $lin $((c - 6)); echo Gezuar Krishtlindjet!
+tput cup $((lin + 1)) $((c - 10)); echo Dhe kodim te mbare ne $new_year
 let c++
 k=1
 
 # Lights and decorations
 while true; do
-    for ((i=1; i<=35; i++)) {
+    for ((i=1; i<=65; i++)) {
         # Turn off the lights
         [ $k -gt 1 ] && {
             tput setaf 2; tput bold
             tput cup ${line[$[k-1]$i]} ${column[$[k-1]$i]}; echo \*
             unset line[$[k-1]$i]; unset column[$[k-1]$i]  # Array cleanup
         }
-
-        li=$((RANDOM % 9 + 3))
+        li=$((RANDOM % 23 + 3))
         start=$((c-li+2))
         co=$((RANDOM % (li-2) * 2 + 1 + start))
         tput setaf $color; tput bold   # Switch colors
@@ -57,10 +57,10 @@ while true; do
         column[$k$i]=$co
         color=$(((color+1)%8))
         # Flashing text
-        sh=1
-        for l in C O D I G O
+        sh=-7
+        for l in kodim
         do
-            tput cup $((lin+1)) $((c-3+sh))
+            tput cup $((lin+1)) $((c+sh))
             echo $l
             let sh++
             sleep 0.01
